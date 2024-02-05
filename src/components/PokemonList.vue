06b089a4ab1e7bcab1e0d35ed2b8031af95270c8
Jirayu Ch <neo.tdot@gmail.com>
2024-02-05 22:20:14 +0700

    update list

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th >#</th>
+        <th class="text-left">ชื่อโปเกมอน</th>
+        <th >ข้อมูล</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in (dataPokemon || [])" :key="item.name">
+        <td>{{ index + 1 }}</td>
+        <td>{{ item.name }}</td>
+        <td><a target="_blank" :href="item.url"><v-btn>ดูข้อมูล</v-btn></a></td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+
+<script>
+export default {
+  props: {
+    dataPokemon: Array
+  }
+}
+</script>
+
+<style></style>
--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,14 +1,34 @@
 <script >
+import PokemonList from '../components/PokemonList.vue';
+import axios from 'axios';
+
+
 export default {
+  components: {
+    PokemonList
+  },
   data() {
     return {
-      name: 'Pokemon !'
+      name: 'Pokemon !',
+      dataPokemon : []
     }
   },
   methods: {
     greet(str) {
       alert(`Hello ${str}!`)
+    },
+    getPokemonAll() {
+      axios.get(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`)
+      .then(res => {
+        this.dataPokemon = res.data.results
+      })
+      .catch(err => {
+        console.error(err); 
+      })
     }
+  },
+  mounted() {
+    this.getPokemonAll()
   }
 }
 </script>
@@ -19,13 +39,18 @@ export default {
 
 
     <v-btn
-     
+
     :block="true"
     color='success'
     @click="greet('Pokemon')"
 
-
     > Button </v-btn>
+
+    <div style="margin-top: 2%;"></div>
+
+    <hr />
+
+    <PokemonList :dataPokemon="dataPokemon" ></PokemonList>
 
 
   </div>
