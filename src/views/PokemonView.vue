b2294117ea0941c44faf05bd3542297b6e93c1bc
Jirayu Ch <neo.tdot@gmail.com>
2024-02-05 21:12:02 +0700

    Initial commit

---
+++ src/views/PokemonView.vue
@@ -0,0 +1,40 @@
+<script >
+export default {
+  data() {
+    return {
+      name: 'Pokemon !'
+    }
+  },
+  methods: {
+    greet(str) {
+      alert(`Hello ${str}!`)
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>This is an {{ name }}</h1>
+
+
+    <v-btn
+     
+    :block="true"
+    color='success'
+    @click="greet('Pokemon')"
+
+
+    > Button </v-btn>
+
+
+  </div>
+
+
+
+</template>
+
+<style>
+
+
+</style>
